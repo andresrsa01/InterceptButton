4a922d9ac4fc4eb0d2623467730fafcced9029cf
851265601 <851265601@qq.com>
2019-05-24 14:59:14 +0800

    Add files via upload

---
+++ InterceptButton/InterceptButton/InterceptButton.Android/BluetoothClicker_Droid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using InterceptButton.Droid;
+
+[assembly: Xamarin.Forms.Dependency(typeof(BluetoothClicker_Droid))]
+namespace InterceptButton.Droid
+{
+    public class BluetoothClicker_Droid : IBluetoothClicker
+    {
+
+        public BluetoothClicker_Droid()
+        {
+        }
+
+        public object GetVolumeOverride()
+        {
+            //CameraButton_Clicked = true;
+
+            return true;
+
+        }
+    }
+}
---
+++ InterceptButton/InterceptButton/InterceptButton/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Android.Graphics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace InterceptButton
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            CameraImage.Source = "CameraImage.png";
+            TakePhotoButton.Clicked += CameraButton_Clicked; //opens camera
+
+            Func<object> func = () =>
+            {
+                var obj = DependencyService.Get<IBluetoothClicker>().GetVolumeOverride();
+                //obj += CameraButton_Clicked;
+                return obj;
+            };
+
+            MessagingCenter.Subscribe<App, string>(App.Current, "OpenPage", (snd, arg) =>
+            {
+                Device.BeginInvokeOnMainThread(() => {
+                    CameraImage.Source = arg;
+                });
+            });
+        }
+
+        private void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
